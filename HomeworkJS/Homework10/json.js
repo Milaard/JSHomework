ab7ed5673f7d26719b17e49fa3cfb981248549dc
Milaard <116643003+Milaard@users.noreply.github.com>
2023-01-27 10:08:36 +0300

    Added Homework 10

---
+++ HomeworkJS/Homework10/json.js
@@ -0,0 +1,45 @@
+const dataInfo = `[
+    {
+        "id": "af1",
+        "width": 250,
+        "url": "https://randomfox.ca/images/60.jpg",
+        "name": "White",
+        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, accusamus!"
+    },
+    {
+        "id": "af2",
+        "width": 250,
+        "url": "https://randomfox.ca/images/70.jpg",
+        "name": "Winter",
+        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, accusamus!"
+    },
+    {
+        "id": "af3",
+        "width": 250,
+        "url": "https://randomfox.ca/images/97.jpg",
+        "name": "Cunning hunter",
+        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, accusamus!"
+    },
+    {
+        "id": "af4",
+        "width": 250,
+        "url": "https://randomfox.ca/images/51.jpg",
+        "name": "Fennecus",
+        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, accusamus!"
+    },
+    {
+        "id": "af5",
+        "width": 250,
+        "url": "https://randomfox.ca/images/78.jpg",
+        "name": "Foxes",
+        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, accusamus!"
+    },
+    {
+        "id": "af6",
+        "width": 250,
+        "url": "https://randomfox.ca/images/46.jpg",
+        "name": "Little fox",
+        "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officiis, accusamus!"
+    }
+]`
+
